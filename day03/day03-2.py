a4f6e51ceecdcc84d0f7db8f13ac3d1ebe4fb3cc
dwaris <dwaris@daum.net>
2022-12-03 12:00:23 +0100

    day03: cleanup group function

--- day03/day03-2.py
+++ day03/day03-2.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 with open('input.txt') as f:
     data = f.read().splitlines()
 
@@ -11,17 +9,9 @@ for i, char in enumerate(range(ord('a'), ord('z') + 1), start=1):
 for i, char in enumerate(range(ord('A'), ord('Z') + 1), start=27):
 	mapped_items[chr(char)] = i
 
+grouped_items = [[data[i], data[i+1], data[i+2]] for i in range(0, len(data), 3)]
 
-grouped_items = defaultdict(list)
-
-group_num = 0
-for i, line in enumerate(data):
-	if not i % 3:
-		group_num += 1
-
-	grouped_items[group_num].append(line)
-
-for triplet in grouped_items.values():
+for triplet in grouped_items:
 	item_sets = [set(x) for x in triplet]
 	result.append(sum([mapped_items[x] for x in set.intersection(*item_sets)]))
 
