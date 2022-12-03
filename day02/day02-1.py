8d6cfa8b22bb94fb45ac6d2cc39891af7ab70565
dwaris <dwaris@daum.net>
2022-12-03 10:59:10 +0100

    day02: cleanup input read

--- day02/day02-1.py
+++ day02/day02-1.py
@@ -1,15 +1,15 @@
 with open('input.txt') as f:
-    lines = [line.rstrip() for line in f]
+    data = f.read().splitlines()
 
 mapped_values = { 'A': 1,
                   'B': 2,
                   'C': 3,
                   'X': 1,
                   'Y': 2,
-                  'Z': 3 }  
+                  'Z': 3 }
 result = 0
 
-for game in lines:
+for game in data:
     p1, p2 = game.split(' ')
 
     x = (mapped_values[p1] - mapped_values[p2]) % 3
@@ -18,7 +18,7 @@ for game in lines:
 
     if x == 2:
       result += 6
-    
+
     elif x == 0:
       result += 3
 
--- day02/day02-2.py
+++ day02/day02-2.py
@@ -1,23 +1,23 @@
 with open('input.txt') as f:
-    lines = [line.rstrip() for line in f]
+    data = f.read().splitlines()
 
 mapped_values = { 'A': 1,
                   'B': 2,
                   'C': 3,
                   'X': 1,
                   'Y': 2,
-                  'Z': 3 }  
+                  'Z': 3 }
 result = 0
 
-for game in lines:
+for game in data:
     p1, p2 = game.split(' ')
 
     if mapped_values[p2] == 2:
       result += mapped_values[p1] + 3
-    
+
     elif mapped_values[p2] == 3:
       result += mapped_values[p1] % 3 + 7
-    
+
     else:
       result += (mapped_values[p1] - 2) % 3 + 1
 
